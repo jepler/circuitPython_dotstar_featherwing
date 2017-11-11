62d44b1fe0c120a2dea820a4f3495f23ff8c5365
Dave Astels <dastels@daveastels.com>
2017-11-10 21:12:23 -0500

    Simplify

--- examples/shifting_in_string/main.py
+++ examples/shifting_in_string/main.py
@@ -1,13 +1,11 @@
 import board
 import dotstar_featherwing
 import time
+import font3
 
-wing = dotstar_featherwing.DotstarFeatherwing(board.D13, board.D11)
+wing = dotstar_featherwing.DotstarFeatherwing(board.D13, board.D11, 0.1)
 
 while True:
 	wing.clear()
-	wing.shift_in_string(wing.font_3, "abcdefghijklmnopqrstuvwxyz0123456789!?.-_+/*=@", (0x00, 0x20, 0x20), 0.1)
-	time.sleep(2)
-	wing.clear()
-	wing.shift_in_string(wing.font_3, "hello adafruit discord!", (0x20, 0x00, 0x20), 0.05)
+	wing.shift_in_string(font3.font, "hello adafruit discord!", (0x20, 0x00, 0x20), 0.05)
 	time.sleep(2)
