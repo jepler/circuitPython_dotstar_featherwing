2c6f50e946b12d26858ab2cf79b41790a16f7340
Dave Astels <dastels@daveastels.com>
2018-01-22 17:36:38 -0500

    New features and related optimizations to make it fit
    - display score on dotstar wing
    - score increment every 25 steps rather than 10
    - 10% speed increase every 100 steps
    - add some comments

--- examples/gauntlet_game/main.py
+++ examples/gauntlet_game/main.py
@@ -24,8 +24,8 @@
 A gaunlet running game using the dotstar wing and the joy wing.
 """
 
-import time
-import random
+from time import sleep
+from random import randint
 
 import board
 import busio
@@ -34,70 +34,88 @@ import Adafruit_seesaw
 
 i2c = busio.I2C(board.SCL, board.SDA)
 ss = Adafruit_seesaw.Seesaw(i2c)
-wing = dotstar_featherwing.DotstarFeatherwing(board.D13, board.D11)
+wing = dotstar_featherwing.DotstarFeatherwing(board.D13, board.D11, 0.1)
 
-black = (0, 0, 0)
-background = (32, 8, 0)
-blue = (0, 0, 64)
-player = (0, 255, 0)
+black = 0x000000
+wall = 0x200800           # must not have any blue, must have red
+pellet = 0x000040               # must have blue
+player = 0x00FF00
 
-row = (background, background, background, background,
-       background, background, background, background,
+numbers = {
+    ' ': [0,  0,  0],
+    '0': [30, 33, 30],
+    '1': [34, 63, 32],
+    '2': [50, 41, 38],
+    '3': [33, 37, 26],
+    '4': [ 7,  4, 63],
+    '5': [23, 37, 25],
+    '6': [30, 41, 25],
+    '7': [49,  9,  7],
+    '8': [26, 37, 26],
+    '9': [38, 41, 30],
+}
+
+row = (wall, wall, wall, wall,
+       wall, wall, wall, wall,
        black, black, black, black, black,
-       background, background, background, background,
-       background, background, background, background)
+       wall, wall, wall, wall,
+       wall, wall, wall, wall)
 
 
 def run():
     """Play the game."""
 
-    player_position_col = 6
+    player_x = 6
     score = 0
     steps = 0
-
-    for _ in range(wing.rows):
-        wing.shift_into_top(row, 4)
-        wing.show()
-
+    step_delay = 0.15
     offset = 4
 
-    while True:
-        wing.set_color(3, player_position_col, black)
-        offset = min(max(0, offset + random.randint(-1, 1)), 9)
+    for _ in range(wing.rows):
         wing.shift_into_top(row, offset)
-        if random.randint(1, 20) == 1:
-            pos = random.randint(8, 12) - offset
-            wing.set_color(0, pos, blue)
+    wing.show()
 
+
+    while True:
+        # remove player sprite
+        wing.set_color(3, player_x, black)
+
+        # shift/advance the track
+        offset = min(max(0, offset + randint(-1, 1)), 9)
+        wing.shift_into_top(row, offset)
+
+        # Maybe add a pellet
+        if randint(1, 20) == 1:
+            wing.set_color(0, randint(8, 12) - offset, pellet)
+
+        # Adjust player position
         joy_x = ss.analog_read(3)
-        if joy_x < 256 and player_position_col > 0:
-            player_delta = -1
-        elif joy_x > 768 and player_position_col < 11:
-            player_delta = 1
-        else:
-            player_delta = 0
-        player_position_col += player_delta
+        if joy_x < 256 and player_x > 0:
+            player_x -= 1
+        elif joy_x > 768 and player_x < 11:
+            player_x += 1
 
-        under_player = wing.get_color(3, player_position_col)
-        if under_player == background:
-            return steps, score
-        elif under_player == blue:
+        # Check for collisions
+        r, _, b = wing.get_color(3, player_x)
+        if b:
             score += 1
+        elif r:
+            return steps, score
 
-        wing.set_color(3, player_position_col, player)
+        # Show player sprite
+        wing.set_color(3, player_x, player)
+
+        # Update some things and sleep a bit
         wing.show()
         steps += 1
-        if steps % 10 == 0:
+        if steps % 25 == 0:
             score += 1
-        time.sleep(0.1)
+        if steps % 100 == 0:
+            step_delay *= 0.9
+        sleep(step_delay)
 
 while True:
     result = run()
-    # got here because of a crash, so report and restart
-    print 'Score: {} Steps: {}'.format(result[1], result[0])
-    wing.clear()
-    wing.show()
-    wing.fill((255, 0, 0))
-    wing.show()
-    wing.clear()
-    wing.show()
+    # got here because of a crash, so report score and restart
+    wing.shift_in_string(numbers, '{:03d}'.format(result[1]), 0x101010)
+    sleep(5)
